e5a58f5e4939cc441d0df23cbe78f800373b154b
Alexey Karaev <avkar.moscow@gmail.com>
2025-02-16 17:22:45 +0300

    gcc fix

--- 3_anvil.sh
+++ 3_anvil.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
-apt update && apt install build-essential tmux -y
+apt update && apt install build-essential -y
 curl -L https://foundry.paradigm.xyz | bash
 docker stop infernet-anvil
 echo 'export PATH="$PATH:/root/.foundry/bin"' >> /etc/profile
