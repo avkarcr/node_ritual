bd3669b08a5d1f051720e3070e6a0eabf8805afb
Alexey Karaev <avkar.moscow@gmail.com>
2025-02-16 16:56:57 +0300

    anvil add 3

--- 2_hello_world.sh
+++ 2_hello_world.sh
@@ -16,3 +16,4 @@ pip install infernet-client
 infernet-cli config other --skip
 infernet-cli add-service hello-world --skip
 infernet-cli start
+exit()
--- 3_anvil.sh
+++ 3_anvil.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-tmux detach && tmux kill-server
 cd ~
 curl -L https://foundry.paradigm.xyz | bash
 docker stop infernet-anvil
