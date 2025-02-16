24d9fa9bd1b653062c1094aa4e7ba3d7833272a9
Alexey Karaev <avkar.moscow@gmail.com>
2025-02-16 17:20:34 +0300

    deployed contract native

--- 3_anvil.sh
+++ 3_anvil.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ~
+apt update && apt install build-essential tmux -y
 curl -L https://foundry.paradigm.xyz | bash
 docker stop infernet-anvil
+echo 'export PATH="$PATH:/root/.foundry/bin"' >> /etc/profile
+source ~/.bashrc
 foundryup
